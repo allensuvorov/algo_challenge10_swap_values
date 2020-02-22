7e52a78797adc04fae273e9974d9090fc3226663
Allen <allenvs@mail.ru>
2020-02-22 22:45:35 +0300

    push shorter variable names and wrapped all into function to make code all even easier to read

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var array = [1,5,10,-2];
-var last = array.length-1;
-[array[0],array[last]]=[array[last],array[0]];
-console.log (array);
+function swap (a) {
+    var l = a.length-1;
+    [a[0],a[l]]= [a[l],a[0]];
+    console.log (a);
+};
+swap(array);
