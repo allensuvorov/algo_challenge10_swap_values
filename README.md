0d3bfdc912e257202a3e00e8a54ed090d08113ae
Allen <allenvs@mail.ru>
2020-02-22 22:25:12 +0300

    added variable LAST to make code easier to read

---
+++ README.md
@@ -0,0 +1,1 @@
+Write a function that will swap the first and last values of any given array. The default minimum length of the array is 2. (e.g. [1,5,10,-2] will become [-2,5,10,1]).
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 var array = [1,5,10,-2];
-[array[0],array[array.length-1]]=[array[array.length-1],array[0]];
+var last = array.length-1;
+[array[0],array[last]]=[array[last],array[0]];
 console.log (array);
